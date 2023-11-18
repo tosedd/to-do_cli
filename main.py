3579bca48ea7e6f50a85d5afe601b396d7a8a04e
tosedd <147439120+tosedd@users.noreply.github.com>
2023-11-18 07:23:54 +0100

    Update main.py
    Fixed innapropriate file path

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-# CLI To-Do script
-# Todor Todorovski 2023
-''' 1: The programme on starts shows the previously added to-do's (if any). In the case of none, prints a sentence.
-    2: The programme waits 8-10sec. and prompts the user if he wants to add a new todo(y/n). Prompts is separated graphically
-    from the first function.
-    3:If yes>prompt to enter and add the todo to the list. If no>"Bye".
-    >>The programme stores its data on a separate file
-    4: The programme automatically sets int values to every task created [task [1], task [2]...]
-'''
 import time
 import csv
 import pandas
